5792554f7580e797ae45fb8a2f2778f44842cb10
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-08-05 01:44:34 +0400

    Add MenuItem and MenuCategory Models

--- Restaurant Ordering & Table Reservation/Team5/Models/MenuCategory.cs
+++ Restaurant Ordering & Table Reservation/Team5/Models/MenuCategory.cs
@@ -2,5 +2,9 @@
 {
     public class MenuCategory
     {
+        public int MenuCategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
