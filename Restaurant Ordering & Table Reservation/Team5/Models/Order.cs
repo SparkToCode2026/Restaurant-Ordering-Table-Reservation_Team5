c9fbbd1b96b0282932722095d69ca07af20a24fc
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-08-05 12:34:41 +0400

    Add the Order relationships

--- Restaurant Ordering & Table Reservation/Team5/Models/Order.cs
+++ Restaurant Ordering & Table Reservation/Team5/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Team5.Models
 {
@@ -18,5 +19,37 @@ namespace Team5.Models
         public string Status { get; set; }         
         public DateTime OrderDate { get; set; }
         public decimal TotalAmount { get; set; }
+
+
+
+        //////Relationships
+
+
+
+        // One User can have many Orders (User 1 - M Order)
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+
+        // One Table can have many Orders (Table 1 - M Order)
+
+        [ForeignKey("Table")]
+        public int TableId { get; set; }
+
+        public Table Table { get; set; }
+
+
+        // One Order can contain many Order Items (Order 1 - M OrderItem)
+        public List<OrderItem> OrderItems { get; set; }
+
+
+        // One Order can have many Payments ( Order 1 - M Payment)
+        public List<Payment> Payments { get; set; }
+
+
+        // One Order can receive many Reviews (Order 1 - M Review)
+        public List<Review> Reviews { get; set; }
     }
 }
