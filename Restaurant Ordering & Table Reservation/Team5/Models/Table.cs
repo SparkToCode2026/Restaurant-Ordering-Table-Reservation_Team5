2ebf376b4e83dc660ffaf1054a76b03e4bbade42
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-08-05 12:16:29 +0400

    Add the Primary Key

--- Restaurant Ordering & Table Reservation/Team5/Models/Table.cs
+++ Restaurant Ordering & Table Reservation/Team5/Models/Table.cs
@@ -4,6 +4,17 @@ namespace Team5.Models
 {
     public class Table
     {
-       
+        // Primary Key
+        [Key]
+        public int Id { get; set; }
+
+        // Table Information
+        public string TableNumber { get; set; }
+
+        public int Capacity { get; set; }
+
+        public string Location { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
