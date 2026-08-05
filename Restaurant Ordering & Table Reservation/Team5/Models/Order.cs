85eb04429be79c1f78ba5593f10d9c873b75fdb1
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-08-05 12:24:19 +0400

    Add the Order primary key

--- Restaurant Ordering & Table Reservation/Team5/Models/Order.cs
+++ Restaurant Ordering & Table Reservation/Team5/Models/Order.cs
@@ -1,7 +1,22 @@
-﻿namespace Team5.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Team5.Models
 {
     public class Order
     {
-       
+        // Primary Key
+        [Key]
+        public int Id { get; set; }
+
+        // Order Information
+
+        // DineIn or Takeaway
+        public string OrderType { get; set; }
+
+        // Pending, Preparing, Ready
+        public string Status { get; set; }         
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
     }
 }
