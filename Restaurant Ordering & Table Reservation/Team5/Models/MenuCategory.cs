2253abffddaa986f4be50cbc326b13d86ffd68a5
Abdullah Albrashdi <albrashdi.abdo@gmail.com>
2026-08-05 15:14:20 +0400

    add the relationship to the user class and fix the menucategory class

--- Restaurant Ordering & Table Reservation/Team5/Models/MenuCategory.cs
+++ Restaurant Ordering & Table Reservation/Team5/Models/MenuCategory.cs
@@ -1,7 +1,10 @@
-﻿namespace Team5.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Team5.Models
 {
     public class MenuCategory
     {
+        [Key]
         public int MenuCategoryId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
--- Restaurant Ordering & Table Reservation/Team5/Models/User.cs
+++ Restaurant Ordering & Table Reservation/Team5/Models/User.cs
@@ -20,6 +20,21 @@ namespace Team5.Models
         public DateTime CreatedAt { get; set; }
 
 
+
+        //Relationships 
+        
+        //One User can have many Reservations (User 1 - M Reservation)
+        public List<Reservation> Reservations { get; set; } 
+        
+        // One User can have many Orders (User 1 - M Order)
+        public List<Order> Orders { get; set; } 
+        
+        // One User can have many Reviews (User 1 - M Review)
+        public List<Review> Reviews { get; set; } 
+        
+        
+
+
     }
 }
 
