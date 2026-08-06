cc052bd74bf039cd79329caf9485cfb0c62f8a8c
Ahad7705 <ii2804679@gmail.com>
2026-08-06 09:27:18 +0400

    add the UserController

--- Restaurant Ordering & Table Reservation/Team5/Controllers/UserController.cs
+++ Restaurant Ordering & Table Reservation/Team5/Controllers/UserController.cs
@@ -1,6 +1,74 @@
-﻿namespace Team5.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Team5.Models;
+
+namespace Team5.Controllers
 {
-    public class UserController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private readonly ProjectContext _context;
+
+        public UserController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // Get All Users
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(_context.Users.ToList());
+        }
+
+        // Get User By Id
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        // Add User
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            return Ok(user);
+        }
+
+        // Update User
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, User user)
+        {
+            if (id != user.UserId)
+                return BadRequest();
+
+            _context.Users.Update(user);
+            _context.SaveChanges();
+
+            return Ok(user);
+        }
+
+        // Delete User
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+                return NotFound();
+
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
