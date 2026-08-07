350bbc60a27007623ea8dcd448312cca37da9570
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-08-07 07:24:21 -0700

    Add MenuItemController with ProjectContext injection

--- Restaurant Ordering & Table Reservation/Team5/Controllers/MenuItemController.cs
+++ Restaurant Ordering & Table Reservation/Team5/Controllers/MenuItemController.cs
@@ -1,6 +1,17 @@
-﻿namespace Team5.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Team5.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class MenuItemController
     {
+        private readonly ProjectContext context;
+
+        public MenuItemController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
     }
 }
