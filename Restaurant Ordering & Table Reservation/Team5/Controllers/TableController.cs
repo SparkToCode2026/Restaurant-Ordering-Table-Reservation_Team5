b007459eaaab3b5355be83002392a1b602e789dd
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-08-08 03:40:38 +0400

    POST - Add a new Table

--- Restaurant Ordering & Table Reservation/Team5/Controllers/TableController.cs
+++ Restaurant Ordering & Table Reservation/Team5/Controllers/TableController.cs
@@ -1,9 +1,32 @@
 ﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Team5.Models;
 
 namespace Team5.Controllers
 {
-    
-    public class TableController
+    [ApiController]
+    [Route("Table")]
+    public class TableController : ControllerBase
     {
+        private ProjectContext context;
+
+        public TableController(ProjectContext _context)
+        {
+            context = _context;
+        }
+
+        // 1. POST - Add a new Table
+        [HttpPost("AddTable")]
+        public IActionResult AddTable(Table table)
+        {
+            context.Tables.Add(table);
+            context.SaveChanges();
+
+            return Ok(table.TableId);
+        }
+
+
+
     }
 }
