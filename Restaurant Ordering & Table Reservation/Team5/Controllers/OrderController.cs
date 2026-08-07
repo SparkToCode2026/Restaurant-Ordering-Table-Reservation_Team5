7b17f73d6fcac097497f6e023bdb02e73a8f936d
AmaniAlAbri8 <amani.alabri8@gmail.com>
2026-08-08 03:32:11 +0400

    PUT - Update the Order

--- Restaurant Ordering & Table Reservation/Team5/Controllers/OrderController.cs
+++ Restaurant Ordering & Table Reservation/Team5/Controllers/OrderController.cs
@@ -25,5 +25,33 @@ namespace Team5.Controllers
 
             return Ok(order.OrderId);
         }
+
+        // 2. PUT - Update the Order
+        [HttpPut("UpdateOrder")]
+        public IActionResult UpdateOrder(int id, Order newOrder)
+        {
+            Order order = context.Orders.FirstOrDefault(o => o.OrderId == id);
+
+            if (order == null)
+            {
+                return NotFound("Order Not Found");
+            }
+
+            order.OrderType = newOrder.OrderType;
+            order.Status = newOrder.Status;
+            order.OrderDate = newOrder.OrderDate;
+            order.TotalAmount = newOrder.TotalAmount;
+            order.UserId = newOrder.UserId;
+            order.TableId = newOrder.TableId;
+
+            context.SaveChanges();
+
+            return Ok("Order Updated Successfully");
+        }
+
+
+
+
+
     }
 }
