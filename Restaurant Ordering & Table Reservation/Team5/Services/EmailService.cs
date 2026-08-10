8b98efd1a93e4c1263c2bff02f39393b20e5b673
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-08-10 10:36:31 -0700

    Add SMTP connection and send logic to EmailService

--- Restaurant Ordering & Table Reservation/Team5/Services/EmailService.cs
+++ Restaurant Ordering & Table Reservation/Team5/Services/EmailService.cs
@@ -21,8 +21,19 @@ namespace Team5.Services
             message.To.Add(new MailboxAddress(toName, toEmail));
             message.Subject = subject;
             message.Body = new TextPart("html") { Text = body };
+
+            using var client = new SmtpClient();
+            client.Connect(
+                _configuration["EmailSettings:SmtpServer"],
+                int.Parse(_configuration["EmailSettings:Port"]),
+                MailKit.Security.SecureSocketOptions.StartTls
+            );
+            client.Authenticate(
+                _configuration["EmailSettings:SenderEmail"],
+                _configuration["EmailSettings:AppPassword"]
+            );
+            client.Send(message);
+            client.Disconnect(true);
         }
-
-
     }
 }
