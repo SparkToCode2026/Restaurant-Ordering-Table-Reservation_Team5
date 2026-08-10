fe45bb55ca7adaacb0149823f8c6b7c4e517f2be
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-08-10 10:32:03 -0700

    Add EmailService constructor with IConfiguration

--- Restaurant Ordering & Table Reservation/Team5/Services/EmailService.cs
+++ Restaurant Ordering & Table Reservation/Team5/Services/EmailService.cs
@@ -2,6 +2,11 @@
 {
     public class EmailService
     {
+        private readonly IConfiguration _configuration;
 
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
     }
 }
